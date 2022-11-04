a544535e05c2af26d4a8e4215fd5a9877518e3b8
RLRama <rl.ramiro11@gmail.com>
2022-11-04 04:22:51 -0300

    New code

--- 0_💬_Chatbot_en_Python.py
+++ 0_💬_Chatbot_en_Python.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import nltk
+import json
 import numpy as np
-import tensorflow as tensorF
+import tensorflow as tf
 from streamlit_chat import message
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras import Sequential
@@ -10,24 +11,6 @@ from tensorflow.keras.layers import Dense, Dropout
 nltk.download("punkt")
 nltk.download("wordnet")
 
-lm = WordNetLemmatizer() # para obtener palabras
-# listas
-ourClasses = []
-newWords = []
-documentX = []
-documentY = []
-# cada intención es tokenizada en palabras y los patrones y sus etiquetas asociadas se añaden a sus listas respectivas
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        ournewTkns = nltk.word_tokenize(pattern)
-        newWords.extend(ournewTkns)
-        documentX.append(pattern)
-        documentY.append(intent["tag"])
+data_file = open("/intents.json").read()
+data = json.loads(data_file)
 
-# añade etiquetas inexistentes a sus clases respectivas
-    if intent["tag"] not in ourClasses:
-        ourClasses.append(intent["tag"])
-
-newWords = [lm.lemmatize(word.lower()) for word in newWords if word not in string.punctuation] # set words to lowercase if not in punctuation
-newWords = sorted(set(newWords)) # ordena palabras
-ourClasses = sorted(set(ourClasses)) # ordena clases
