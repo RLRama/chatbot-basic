b0c57caab8cad67a1193ffe5e371534227c9717b
RLRama <rl.ramiro11@gmail.com>
2022-11-04 04:23:59 -0300

    New code

--- 0_💬_Chatbot_en_Python.py
+++ 0_💬_Chatbot_en_Python.py
@@ -11,6 +11,6 @@ from tensorflow.keras.layers import Dense, Dropout
 nltk.download("punkt")
 nltk.download("wordnet")
 
-data_file = open("/intents.json").read()
+data_file = open("intents.json").read()
 data = json.loads(data_file)
 
