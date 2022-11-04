7d5e81db1f8b8dd0976d4fa3f69938e5a9db84e0
RLRama <rl.ramiro11@gmail.com>
2022-11-04 05:57:33 -0300

    New code

--- 0_💬_Chatbot_en_Python.py
+++ 0_💬_Chatbot_en_Python.py
@@ -7,8 +7,8 @@ from matplotlib.pyplot import text
 
 @st.cache(hash_funcs={transformers.models.gpt2.tokenization_gpt2_fast.GPT2TokenizerFast: hash}, suppress_st_warning=True)
 def load_data():    
-    tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-    model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+    tokenizer = AutoTokenizer.from_pretrained("facebook/blenderbot-400M-distill")
+    model = AutoModelForCausalLM.from_pretrained("facebook/blenderbot-400M-distill")
     return tokenizer, model
 
 tokenizer, model = load_data()
@@ -33,11 +33,7 @@ else:
 new_user_input_ids = tokenizer.encode(input + tokenizer.eos_token, return_tensors='pt')
 
 bot_input_ids = torch.cat([st.session_state.chat_history_ids, new_user_input_ids], dim=-1) if st.session_state.count > 1 else new_user_input_ids
-
-
 st.session_state.chat_history_ids = model.generate(bot_input_ids, max_length=5000, pad_token_id=tokenizer.eos_token_id)
-
-
 response = tokenizer.decode(st.session_state.chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
 
 if st.session_state.old_response == response:
