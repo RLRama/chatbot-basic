5bfc6e1e2302d0cb201236978b46c211d7e647bd
RLRama <rl.ramiro11@gmail.com>
2022-11-10 23:46:57 -0300

    Comments

--- pages/1_🎞️_Visión de máquina.py
+++ pages/1_🎞️_Visión de máquina.py
@@ -22,51 +22,48 @@ uploaded_file = st.file_uploader("Subir archivo de imagen")
 
 def asciiart(in_f, SC, GCF,  out_f, color1='black', color2='blue', bgcolor='white'):
 
-    # The array of ascii symbols from white to black
+    # El arreglo de símbolos ASCII yendo de blanco a negro
     chars = np.asarray(list(' .,:irs?@9B&#'))
 
-    # Load the fonts and then get the the height and width of a typical symbol 
-    # You can use different fonts here
+    # Cargar de fuentes y tamaño (ancho y alto) de un símbolo común. Podemos usar distintas fuentes acá
     font = ImageFont.load_default()
     letter_width = font.getsize("x")[0]
     letter_height = font.getsize("x")[1]
 
     WCF = letter_height/letter_width
 
-    #open the input file
+     # Abrir el archivo de entrada
     img = Image.open(in_f)
 
 
-    #Based on the desired output image size, calculate how many ascii letters are needed on the width and height
+    # De acuerdo a la imagen de salida, calcular cuántas letras ASCII se necesitan para el ancho y alto
     widthByLetter=round(img.size[0]*SC*WCF)
     heightByLetter = round(img.size[1]*SC)
     S = (widthByLetter, heightByLetter)
 
-    #Resize the image based on the symbol width and height
+    # Cambiar el tamaño de imagen con respecto al ancho y alto de los símbolos
     img = img.resize(S)
     
-    #Get the RGB color values of each sampled pixel point and convert them to graycolor using the average method.
-    # Refer to https://www.johndcook.com/blog/2009/08/24/algorithms-convert-color-grayscale/ to know about the algorithm
+    # Valores de color RGB de punto de pixel de muestra y conversión monocromática usando el método promedio. Más información en https://www.johndcook.com/blog/2009/08/24/algorithms-convert-color-grayscale/
     img = np.sum(np.asarray(img), axis=2)
     
-    # Normalize the results, enhance and reduce the brightness contrast. 
-    # Map grayscale values to bins of symbols
+    # Normalización de resultados, mejorar y reducir el contraste del brillo
     img -= img.min()
     img = (1.0 - img/img.max())**GCF*(chars.size-1)
     
-    # Generate the ascii art symbols 
+    # Generar símbolos de arte ASCII
     lines = ("\n".join( ("".join(r) for r in chars[img.astype(int)]) )).split("\n")
 
-    # Create gradient color bins
+    # Crear bin de color de gradiente
     nbins = len(lines)
 
-    #Create an image object, set its width and height
+    # Crear un objeto imagen, darle ancho y alto
     newImg_width= letter_width *widthByLetter
     newImg_height = letter_height * heightByLetter
     newImg = Image.new("RGBA", (newImg_width, newImg_height), bgcolor)
     draw = ImageDraw.Draw(newImg)
 
-    # Print symbols to image
+    # Imprimir símbolos a la imagen
     leftpadding=0
     y = 0
     lineIdx=0
@@ -77,43 +74,39 @@ def asciiart(in_f, SC, GCF,  out_f, color1='black', color2='blue', bgcolor='whit
         draw.text((leftpadding, y), line, '#0000FF', font=font)
         y += letter_height
 
-    # Save the image file
+    # Guardar el archivo imagen
 
-    #out_f = out_f.resize((1280,720))
     newImg.save(out_f)
 
 
 def load_image(filename, size=(512,512)):
-	# load image with the preferred size
+	# Carga de imagen con el tamaño preferido
 	pixels = load_img(filename, target_size=size)
-	# convert to numpy array
+	# Conversión a arreglo de numpy
 	pixels = img_to_array(pixels)
-	# scale from [0,255] to [-1,1]
+	# Reescalar de [0,255] a [-1,1]
 	pixels = (pixels - 127.5) / 127.5
-	# reshape to 1 sample
+	# Cambiar forma a una muestra
 	pixels = expand_dims(pixels, 0)
 	return pixels
 
 
 def imgGen2(img1):
-  inputf = img1  # Input image file name
+  inputf = img1  # Nombre de archivo de imagen de entrada
 
-  SC = 0.1    # pixel sampling rate in width
-  GCF= 2      # contrast adjustment
+  SC = 0.1    # Tasa de muestreo de píxeles en ancho
+  GCF= 2      # Ajuste de contraste
 
-  asciiart(inputf, SC, GCF, "results.png")   #default color, black to blue
+  asciiart(inputf, SC, GCF, "results.png")   # color predeterminado de negro a azul
   asciiart(inputf, SC, GCF, "results_pink.png","blue","pink")
   img = Image.open(img1)
   img2 = Image.open('results.png').resize(img.size)
-  #img2.save('result.png')
-  #img3 = Image.open('results_pink.png').resize(img.size)
-  #img3.save('resultp.png')
   return img2	
 
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)	
 	
-    st.image(uploaded_file, caption='Input Image', use_column_width=True)
+    st.image(uploaded_file, caption='Imagen de entrada', use_column_width=True)
     im = imgGen2(uploaded_file)	
-    st.image(im, caption='ASCII art', use_column_width=True)
+    st.image(im, caption='Imagen pero en arte ASCII', use_column_width=True)
